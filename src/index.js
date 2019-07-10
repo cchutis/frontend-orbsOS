f39322e7cb24efa273b401a4a7fbfa9e42fac6fb
Constantine Chutis <constantinec84@gmail.com>
2019-07-10 13:07:47 -0400

    Added all create and splash pages

--- src/index.js
+++ src/index.js
@@ -1,72 +1,16 @@
-// const browser = document.querySelector('#browser');
-// const bar = document.querySelector('#browser-bar');
-// const close = document.querySelector('#close');
-// let initX, initY, mousePressX, mousePressY;
-// browser.style.transform = "translate(" + window.innerWidth + "px," + window.innerHeight + "px) scale(0,0)";
-
-// setTimeout(function () {
-//     browser.classList.add('-show')
-//     browser.classList.add('-animating')
-//     let w = window.innerWidth;
-//     let h = window.innerHeight;
-//     let x = Math.floor(Math.random() * w / 2)
-//     let y = Math.floor(Math.random() * h / 2)
-//     browser.dataset.x = x;
-//     browser.dataset.y = y
-//     browser.style.transform = "translate(" + x + "px," + y + "px) scale(1,1)";
-//     setTimeout(function () { browser.classList.remove('-animating') }, 200)
-// }, 500)
-
-// bar.addEventListener('mousedown', function (e) {
-//     initX = browser.dataset.x || browser.offsetLeft;
-//     initY = browser.dataset.y || browser.offsetTop;
-//     mousePressX = event.clientX;
-//     mousePressY = event.clientY;
-//     browser.style.zIndex = 100;
-//     window.addEventListener('mousemove', repositionElement, false);
-//     window.addEventListener('mouseup', function () {
-//         window.removeEventListener('mousemove', repositionElement, false);
-//     }, false);
-// }, false)
-
-// function repositionElement(event) {
-//     var x = Number(initX) + event.clientX - mousePressX;
-//     var y = Number(initY) + event.clientY - mousePressY;
-//     browser.style.transform = "translate(" + x + "px," + y + "px)";
-//     browser.dataset.x = x;
-//     browser.dataset.y = y;
-// }
-
-// close.addEventListener('mousedown', function (e) {
-//     browser.classList.add('-animating')
-//     setTimeout(function () {
-//         let x = browser.dataset.x || browser.offsetLeft;
-//         let y = browser.dataset.y || browser.offsetTop;
-//         browser.style.transform = "translate(" + x + "px," + y + "px) scale(0.001,0.001)"
-//     }, 50)
-//     // auto open when windo.open is called
-//     setTimeout(function () {
-//         window.open()
-//     }, 3000)
-// }, false)
-
-// window.open = function (URL) {
-//     let x = browser.dataset.x || browser.offsetLeft;
-//     let y = browser.dataset.y || browser.offsetTop;
-//     browser.style.transform = "translate(" + x + "px," + y + "px) scale(1,1)"
-//     setTimeout(function () { browser.classList.remove('-animating') }, 200)
-// }
-
 // Time Widget
 const timeWidget = document.querySelector('#desktop-widget')
 function updateTime() {
-    var currentTime = new Date()
-    var hours = currentTime.getHours()
-    var minutes = currentTime.getMinutes()
+    let currentTime = new Date()
+    let hours = currentTime.getHours()
+    let minutes = currentTime.getMinutes()
     if (minutes < 10) {
         minutes = "0" + minutes
     }
-    var t_str = hours + ":" + minutes + " ";
+    // if (hours > 12) {
+    //     hours -= 12
+    // }
+    let t_str = hours + ":" + minutes + " ";
     if (hours > 11) {
         t_str += "PM";
     } else {
@@ -76,6 +20,9 @@ function updateTime() {
 }
 setInterval(updateTime, 1000);
 document.addEventListener("DOMContentLoaded", updateTime())
+
+//Weather Widget
+
 
 // get all draggie elements
 let draggableElems = document.querySelectorAll('.draggable');
@@ -122,6 +69,9 @@ dock.addEventListener('click', function(e) {
     }
     if(e.target.id === 'task-tracker-icon') {
         document.querySelector('#task-tracker-app').style.display = 'block';
+    }
+    if(e.target.id === 'piano-icon') {
+        document.querySelector('#piano-app').style.display = 'block';
     }
 })
 
@@ -244,4 +194,198 @@ document.addEventListener('DOMContentLoaded', function () {
         calculator.operator = null;
     }
 
+})
+
+// Piano App
+
+const c = document.querySelector('#c')
+const c_audio = document.createElement('audio')
+c_audio.src = "audio/middle_c.mp3"
+
+const cSharp = document.querySelector('#c_sharp')
+const c_sharp_audio = document.createElement('audio')
+c_sharp_audio.src = "audio/mid_c_sharp.mp3"
+
+const d = document.querySelector('#d')
+const d_audio = document.createElement('audio')
+d_audio.src = "audio/middle_d.mp3"
+
+const dSharp = document.querySelector('#d_sharp')
+const d_sharp_audio = document.createElement('audio')
+d_sharp_audio.src = "audio/mid_d_sharp.mp3"
+
+const e = document.querySelector('#e')
+const e_audio = document.createElement('audio')
+e_audio.src = "audio/middle_e.mp3"
+
+const f = document.querySelector('#f')
+const f_audio = document.createElement('audio')
+f_audio.src = "audio/middle_f.mp3"
+
+const fSharp = document.querySelector('#f_sharp')
+const f_sharp_audio = document.createElement('audio')
+f_sharp_audio.src = "audio/mid_f_sharp.mp3"
+
+const g = document.querySelector('#g')
+const g_audio = document.createElement('audio')
+g_audio.src = "audio/middle_g.mp3"
+
+const gSharp = document.querySelector('#g_sharp')
+const g_sharp_audio = document.createElement('audio')
+g_sharp_audio.src = "audio/mid_g_sharp.mp3"
+
+const a = document.querySelector('#a')
+const a_audio = document.createElement('audio')
+a_audio.src = "audio/middle_a.mp3"
+
+const aSharp = document.querySelector('#a_sharp')
+const a_sharp_audio = document.createElement('audio')
+a_sharp_audio.src = "audio/mid_a_sharp.mp3"
+
+const b = document.querySelector('#b')
+const b_audio = document.createElement('audio')
+b_audio.src = "audio/middle_b.mp3"
+
+const highC = document.querySelector('#highC')
+const high_c_audio = document.createElement('audio')
+high_c_audio.src = "audio/high_c.mp3"
+
+const highCSharp = document.querySelector('#highCSharp')
+const high_c_sharp_audio = document.createElement('audio')
+high_c_sharp_audio.src = "audio/high_c_sharp.mp3"
+
+const highD = document.querySelector('#highD')
+const high_d_audio = document.createElement('audio')
+high_d_audio.src = "audio/high_d.mp3"
+
+const highDSharp = document.querySelector('#highDSharp')
+const high_d_sharp_audio = document.createElement('audio')
+high_d_sharp_audio.src = "audio/high_d_sharp.mp3"
+
+const highE = document.querySelector('#highE')
+const high_e_audio = document.createElement('audio')
+high_e_audio.src = "audio/high_e.mp3"
+
+const highF = document.querySelector('#highF')
+const high_f_audio = document.createElement('audio')
+high_f_audio.src = "audio/high_f.mp3"
+
+const highFSharp = document.querySelector('#highFSharp')
+const high_f_sharp_audio = document.createElement('audio')
+high_f_sharp_audio.src = "audio/high_f_sharp.mp3"
+
+const highG = document.querySelector('#highG')
+const high_g_audio = document.createElement('audio')
+high_g_audio.src = "audio/high_g.mp3"
+
+const highGSharp = document.querySelector('#highGSharp')
+const high_g_sharp_audio = document.createElement('audio')
+high_g_sharp_audio.src = "audio/high_g_sharp.mp3"
+
+const highA = document.querySelector('#highA')
+const high_a_audio = document.createElement('audio')
+high_a_audio.src = "audio/high_a.mp3"
+
+const highASharp = document.querySelector('#highASharp')
+const high_a_sharp_audio = document.createElement('audio')
+high_a_sharp_audio.src = "audio/high_a_sharp.mp3"
+
+const highB = document.querySelector('#highB')
+const high_b_audio = document.createElement('audio')
+high_b_audio.src = "audio/high_b.mp3"
+
+c.addEventListener('click', function (e) {
+    c_audio.play()
+})
+
+cSharp.addEventListener('click', function (e) {
+    c_sharp_audio.play()
+})
+
+d.addEventListener('click', function (e) {
+    d_audio.play()
+})
+
+dSharp.addEventListener('click', function (e) {
+    d_sharp_audio.play()
+})
+
+e.addEventListener('click', function (e) {
+    e_audio.play()
+})
+
+f.addEventListener('click', function (e) {
+    f_audio.play()
+})
+
+fSharp.addEventListener('click', function (e) {
+    f_sharp_audio.play()
+})
+
+g.addEventListener('click', function (e) {
+    g_audio.play()
+})
+
+gSharp.addEventListener('click', function (e) {
+    g_sharp_audio.play()
+})
+
+a.addEventListener('click', function (e) {
+    a_audio.play()
+})
+
+aSharp.addEventListener('click', function (e) {
+    a_sharp_audio.play()
+})
+
+b.addEventListener('click', function (e) {
+    b_audio.play()
+})
+
+highC.addEventListener('click', function (e) {
+    high_c_audio.play()
+})
+
+highCSharp.addEventListener('click', function (e) {
+    high_c_sharp_audio.play()
+})
+
+highD.addEventListener('click', function (e) {
+    high_d_audio.play()
+})
+
+highDSharp.addEventListener('click', function (e) {
+    high_d_sharp_audio.play()
+})
+
+highE.addEventListener('click', function (e) {
+    high_e_audio.play()
+})
+
+highF.addEventListener('click', function (e) {
+    high_f_audio.play()
+})
+
+highFSharp.addEventListener('click', function (e) {
+    high_f_sharp_audio.play()
+})
+
+highG.addEventListener('click', function (e) {
+    high_g_audio.play()
+})
+
+highGSharp.addEventListener('click', function (e) {
+    high_g_sharp_audio.play()
+})
+
+highA.addEventListener('click', function (e) {
+    high_a_audio.play()
+})
+
+highASharp.addEventListener('click', function (e) {
+    high_a_sharp_audio.play()
+})
+
+highB.addEventListener('click', function (e) {
+    high_b_audio.play()
 })
